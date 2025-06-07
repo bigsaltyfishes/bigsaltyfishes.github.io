0d2dc9de3b6c03d21aff0a12ac2e7a268cf48ade
bigsaltyfishes <bigsaltyfishes@gmail.com>
2025-06-07 21:55:52 +0800

    fixup! Update integrity_update.py to support url prefix
    * Trunk use absolute url instead of relative path when publish.
    
    Signed-off-by: bigsaltyfishes <bigsaltyfishes@gmail.com>

--- integrity_update.py
+++ integrity_update.py
@@ -13,9 +13,12 @@ def compute_sha384(filepath):
     b64 = base64.b64encode(digest).decode('ascii')
     return f"sha384-{b64}"
 
-def update_integrity(html_path, base_dir=None, inplace=False, output=None):
+def update_integrity(html_path, base_dir=None, inplace=False, url_prefix=None, output=None):
     if base_dir is None:
         base_dir = os.path.dirname(os.path.abspath(html_path))
+
+    if url_prefix.endswith('/'):
+        url_prefix = url_prefix[:-1]
 
     with open(html_path, 'r', encoding='utf-8') as f:
         soup = BeautifulSoup(f, 'html.parser')
@@ -32,7 +35,11 @@ def update_integrity(html_path, base_dir=None, inplace=False, output=None):
         attr = 'href' if tag.name == 'link' else 'src'
         path = tag[attr]
         # Only process paths that start with '/'
-        if path.startswith('/'):
+        path_start = url_prefix if url_prefix != None else '/'
+        
+        if path.startswith(path_start):
+            if url_prefix != None:
+                path = path[len(path_start):]  # Remove the URL prefix if specified
             local_path = os.path.join(base_dir, path.lstrip('/'))
             if not os.path.isfile(local_path):
                 print(f"Warning: file not found {local_path}, skipping.")
@@ -61,6 +68,7 @@ if __name__ == '__main__':
     parser.add_argument('html', help='Path to the HTML file')
     parser.add_argument('-b', '--base-dir', help='Base directory for local files (defaults to HTML directory)')
     parser.add_argument('-i', '--inplace', action='store_true', help='Overwrite the original HTML file')
+    parser.add_argument('-u', '--url', help='URL prefix to match local files')
     parser.add_argument('-o', '--output', help='Path for output HTML (if not using inplace)')
     args = parser.parse_args()
-    update_integrity(args.html, base_dir=args.base_dir, inplace=args.inplace, output=args.output)
+    update_integrity(args.html, base_dir=args.base_dir, inplace=args.inplace, url_prefix=args.url, output=args.output)

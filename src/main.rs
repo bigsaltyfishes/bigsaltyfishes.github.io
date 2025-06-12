940a763b0eaaa567d98847deed4a766de0c8b2bb
bigsaltyfishes <bigsaltyfishes@gmail.com>
2025-06-12 15:52:58 +0800

    chore: Setup panic hook for better debug
    Signed-off-by: bigsaltyfishes <bigsaltyfishes@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ mod router;
 mod types;
 
 fn main() {
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
     wasm_logger::init(wasm_logger::Config::default());
 
     mount_to_body(app::App);
